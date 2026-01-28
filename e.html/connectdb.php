e4e02804d3060c5491d403e6b5fa98059d0434ee
Dongraksa <dongraksa2548@gmail.com>
2026-01-28 16:37:06 +0700

    a

--- e.html/connectdb.php
+++ e.html/connectdb.php
@@ -2,7 +2,7 @@
 
 $host = "localhost";
 			$user = "root";
-			$pwd = "";
+			$pwd = "r660109";
 			$db = "4040db";
 			$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อข้อมูลไม่ได้");
 			mysqli_query($conn, "SET NAMES utf8"); 
